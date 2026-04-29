12a287ae8ac8b83006fcbdc87de14d383bf6cb98
phutawan0407 <95275106+phutawan0407@users.noreply.github.com>
2026-04-29 14:51:12 +0700

    test(Checkout) : add skip

--- testCases/features/checkout/checkout.spec.ts
+++ testCases/features/checkout/checkout.spec.ts
@@ -5,17 +5,17 @@ import { readJson } from "@utils/ResourceLoader"
 const dataTest = readJson('resources/dataTest/checkout/checkoutInfo.json') as any;
 const checkoutInfo = dataTest.checkoutInfo
 
-test.describe('Checkout', () =>{
+test.describe('Checkout', () => {
     test.beforeEach(async ({ loginPage, inventoryPage, cartPage }) => {
         await loginPage.goto(env.BASE_URL)
-        await loginPage.login(env.USERNAME,env.PASSWORD)
+        await loginPage.login(env.USERNAME, env.PASSWORD)
         await inventoryPage.addFirstItemToCart()
         await cartPage.goto()
         await cartPage.clickCheckout()
     })
 
-    test.skip('Verify user can checkout when input checkout valid info correctly', {tag: ["@Regression", "@Smoke", "@High", "@Test"]}, async ({ checkoutPage}) => {
-        await checkoutPage.fillAllCheckoutInfoAndContinue(checkoutInfo.firstName,checkoutInfo.lastName,checkoutInfo.postalCode)
+    test.skip('Verify user can checkout when input checkout valid info correctly', { tag: ["@Regression", "@Smoke", "@High", "@Test"] }, async ({ checkoutPage }) => {
+        await checkoutPage.fillAllCheckoutInfoAndContinue(checkoutInfo.firstName, checkoutInfo.lastName, checkoutInfo.postalCode)
         await checkoutPage.verifyCheckoutStepTwoPage()
     })
 })
